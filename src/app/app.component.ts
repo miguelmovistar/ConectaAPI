846b6e66c613063730220fe0b88a6c1a707549de
Miguel Castillo <miguelcastilloenlinea@gmail.com>
2026-03-31 18:33:56 -0600

    Added title component is not standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ConectaComponent } from './conecta/conecta.component';
+import { TitleComponent } from './title/title.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ConectaComponent],
+  imports: [RouterOutlet, ConectaComponent, TitleComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
